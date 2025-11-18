31a75bf56169576a8197b330b7ab5e214cef9231
Wellington <wndrj7@gmail.com>
2025-11-18 15:58:19 -0300

    feat: criação de token jwt

---
+++ internal/infra/jwt/jwt.go
@@ -0,0 +1,42 @@
+package jwt
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = os.Getenv("JWT_SECRET_KEY")
+
+func CreateToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) error {
+   token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+      return secretKey, nil
+   })
+  
+   if err != nil {
+      return err
+   }
+  
+   if !token.Valid {
+      return fmt.Errorf("invalid token")
+   }
+  
+   return nil
+}
