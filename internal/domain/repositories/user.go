ad88f35797f52daea7ea35617cf81b8deb8a404b
Wellington <wndrj7@gmail.com>
2025-11-18 09:23:10 -0300

    chore: alteracao no repository

--- internal/domain/repositories/user.go
+++ internal/domain/repositories/user.go
@@ -1,10 +1,12 @@
 package repositories
 
 import (
+	"context"
 	"scheduling/internal/domain/entities"
 )
 
 type UserRepository interface {
 	Repository[entities.User]
-	Exists(id int) (bool, error)
+	Exists(ctx context.Context, id int) (bool, error)
+	EmailExist(ctx context.Context, email string) (bool, error)
 }
